28108943d66585f68b2e5a26dcadf6b7c7a8b1c0
aakash018 <aakashkhanal015@gmail.com>
2022-08-15 23:01:39 +0545

    [FIXED] customer layout UI fixes

--- web/components/Customer/Layout.tsx
+++ web/components/Customer/Layout.tsx
@@ -16,7 +16,11 @@ const Layout: React.FC<Props> = ({ children, sidebar, type = "catogry" }) => {
     <>
       <CustomerNav />
       <div className={styles.mainContainer}>
-        <div className={styles.catogriesBarConainer}>
+        <div
+          className={`${styles.catogriesBarConainer} ${
+            type === "catogry" ? styles.lowMargin : ""
+          }`}
+        >
           <CatogriesSideBar sidebar={sidebar} type={type} />
         </div>
         <div className={styles.contentContainer}>{children}</div>
