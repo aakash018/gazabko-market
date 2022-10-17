74f7e2805066b8e351926a8808f7a8a2e77ac1dc
iambigyandahal <iambigyandahal@gmail.com>
2022-10-17 15:10:00 +0545

    update: fixed css and cart page sticky summary

--- web/components/shared/Customer/SearchBar.tsx
+++ web/components/shared/Customer/SearchBar.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import styles from "../../../styles/components/shared/Customer/SearchBar.module.scss";
 import { BsSearch } from "react-icons/bs";
 import Router from "next/router";
+import CategoriesHolderMenu from "../../Customer/CategoriesHolderMenu";
 
 const SearchBarCustomer: React.FC = () => {
   const searchInput = useRef<HTMLInputElement>(null);
@@ -18,18 +19,23 @@ const SearchBarCustomer: React.FC = () => {
   };
 
   return (
-    <div className={styles.searchBar}>
-      <section className={styles.icon}>
-        <BsSearch />
-      </section>
-      <section className={styles.input}>
-        <input
-          type="text"
-          placeholder="Search for products or brands"
-          onKeyDown={handleSearch}
-          ref={searchInput}
-        />
-      </section>
+    <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+      <div style={{ border: "1px solid #888", padding: "2px 5px" }}>
+        <CategoriesHolderMenu />
+      </div>
+      <div className={styles.searchBar}>
+        <section className={styles.icon}>
+          <BsSearch />
+        </section>
+        <section className={styles.input}>
+          <input
+            type="text"
+            placeholder="Search for products or brands"
+            onKeyDown={handleSearch}
+            ref={searchInput}
+          />
+        </section>
+      </div>
     </div>
   );
 };
