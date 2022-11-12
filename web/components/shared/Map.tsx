28e29f07d3df2243031b2d1fc1a7a6fcbdde0025
aakash018 <aakashkhanal015@gmail.com>
2022-11-12 11:18:15 +0545

    [UPDATED] checkout page

--- web/components/shared/Map.tsx
+++ web/components/shared/Map.tsx
@@ -1,7 +1,14 @@
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
 import L from "leaflet";
 
@@ -14,10 +21,9 @@ const center = {
   lng: 85.324,
 };
 
-const Map: React.FC<Props> = ({ setAddress }) => {
+const GetCods: React.FC<Props> = ({ setAddress }) => {
   const markerRef = useRef<L.Marker<any>>(null);
   const [position, setPosition] = useState(center);
-
   const eventHandlers = useMemo(
     () => ({
       dragend() {
@@ -63,22 +69,64 @@ const Map: React.FC<Props> = ({ setAddress }) => {
     []
   );
 
+  useMapEvents({
+    click(e) {
+      console.log(e.latlng);
+
+      setPosition({
+        lat: e.latlng.lat,
+        lng: e.latlng.lng,
+      });
+
+      fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${e.latlng.lat}&lon=${e.latlng.lng}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setAddress(
+            `${
+              data.address.amenity != undefined
+                ? data.address.amenity + ", "
+                : ""
+            }` +
+              `${
+                data.address.neighbourhood != undefined
+                  ? data.address.neighbourhood + ", "
+                  : ""
+              }` +
+              `${
+                data.address.suburb != undefined
+                  ? data.address.suburb + ", "
+                  : ""
+              }` +
+              `${data.address.postcode ? data.address.postcode + "-" : ""}` +
+              data.address.municipality +
+              ", " +
+              data.address.region +
+              ", " +
+              data.address.country
+          );
+        });
+    },
+  });
+
   return (
-    <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+    <Marker position={position} ref={markerRef} draggable={false}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  );
+};
+
+const Map: React.FC<Props> = ({ setAddress }) => {
+  return (
+    <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker
-        position={position}
-        ref={markerRef}
-        draggable={true}
-        eventHandlers={eventHandlers}
-      >
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <GetCods setAddress={setAddress} />
     </MapContainer>
   );
 };
--- web/pages/checkout.tsx
+++ web/pages/checkout.tsx
@@ -1,72 +1,141 @@
 import Link from "next/link";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { BsCash } from "react-icons/bs";
 import Layout from "../components/Customer/Layout";
 import Button from "../components/shared/Button";
-import Intput from "../components/shared/Input";
+import IntputField from "../components/shared/Input";
+import Map from "../components/shared/Map";
 import styles from "../styles/components/Customer/pages/Checkout.module.scss";
 
 const CheckoutPage = () => {
   const deliveryAddress = useRef<HTMLInputElement>(null);
+
+  const [newAddress, setNewAddress] = useState("");
+
+  const [showMap, setShowMap] = useState(false);
 
   return (
     <Layout>
       <div className={styles.checkoutPage}>
         <div className={styles.titleMain}>Checkout</div>
         <div className={styles.content}>
-          <div className={styles.logo}>
-            <BsCash />
-            <span>Cash On Delivery</span>
-          </div>
-          <div className={styles.info}>
-            You can pay in cash to our courier when you receive the goods at
-            your doorstep.
-          </div>
-          <div className={styles.itemsBeingBought}>
-            <div className={styles.title}>Items in the cart</div>
-            <ul>
-              <li>
-                <Link href={"/products/adadsd"}>Manaslu Mg5 Semi</Link>
-              </li>
-              <li>
-                <Link href={"/products/adadsd"}>GoldStar Shoes P302 Black</Link>
-              </li>
-              <li>
-                <Link href={"/products/adadsd"}>Yearcon Black Easy Shoes</Link>
-              </li>
-              <li>
-                <Link href={"/products/adadsd"}>
-                  OLEEV White Dial King/Queen...
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className={styles.paymentInfo}>
-            <div className={styles.orderSummary}>
-              <section className={styles.info}>
-                <section className={styles.data}>
-                  <span className={styles.title}>SubTotal: </span>
-                  <span className={styles.number}>Rs. {2400} </span>
+          <div className={styles.upperPart}>
+            <div className={styles.orderInfo}>
+              <div className={styles.logo}>
+                <BsCash />
+                <span>Cash On Delivery</span>
+              </div>
+              <div className={styles.info}>
+                You can pay in cash to our courier when you receive the goods at
+                your doorstep.
+              </div>
+              <div className={styles.itemsBeingBought}>
+                <div className={styles.title}>Items in the cart</div>
+                <ul>
+                  <li>
+                    <Link href={"/products/adadsd"}>Manaslu Mg5 Semi</Link>
+                  </li>
+                  <li>
+                    <Link href={"/products/adadsd"}>
+                      GoldStar Shoes P302 Black
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/products/adadsd"}>
+                      Yearcon Black Easy Shoes
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/products/adadsd"}>
+                      OLEEV White Dial King/Queen...
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.paymentInfo}>
+              <div className={styles.orderSummary}>
+                <section className={styles.info}>
+                  <section className={styles.data}>
+                    <span className={styles.title}>SubTotal: </span>
+                    <span className={styles.number}>Rs. {2400} </span>
+                  </section>
+                  <section className={styles.data}>
+                    <span className={styles.title}>Shipping fee: : </span>
+                    <span className={styles.number}>Rs. {60} </span>
+                  </section>
+                  <section className={`${styles.data} ${styles.total}`}>
+                    <span className={styles.title}>Total: </span>
+                    <span className={styles.number}>Rs. {2460} </span>
+                  </section>
+                  <section className={styles.actionBtn}>
+                    <Button>CONFIRM ORDER</Button>
+                  </section>
                 </section>
-                <section className={styles.data}>
-                  <span className={styles.title}>Shipping fee: : </span>
-                  <span className={styles.number}>Rs. {60} </span>
-                </section>
-                <section className={`${styles.data} ${styles.total}`}>
-                  <span className={styles.title}>Total: </span>
-                  <span className={styles.number}>Rs. {2460} </span>
-                </section>
-                <div className={styles.addressInfo}>
-                  <Intput input={deliveryAddress} label="Delivery address" />
-                  <Intput input={deliveryAddress} label="Nearest Landmark" />
-                </div>
-                <section className={styles.actionBtn}>
-                  <Button>CONFIRM ORDER</Button>
-                </section>
-              </section>
+              </div>
             </div>
           </div>
+          <div className={styles.addressHolder}>
+            <h3>Default Delivery Address</h3>
+            <div className={styles.addressInfo}>
+              <section>
+                <div className={styles.infoTab}>
+                  <div className={styles.tag}>Phone No.</div>
+                  <div className={styles.info}>9862458712</div>
+                </div>
+                <div className={styles.infoTab}>
+                  <div className={styles.tag}>Email</div>
+                  <div className={styles.info}>randomxyz@gmail.com</div>
+                </div>
+              </section>
+              <section>
+                <div className={styles.infoTab}>
+                  <div className={styles.tag}>Address</div>
+                  <div className={styles.info}>
+                    Deva Arcade, Lut Chok, Thamel, 14292-Kathmandu, Bagmati
+                    Pradesh, Nepal
+                  </div>
+                </div>
+                <div className={styles.infoTab}>
+                  <div className={styles.tag}>Nearest Landmark</div>
+                  <div className={styles.info}>Deva Arcade</div>
+                </div>
+              </section>
+            </div>
+            <div className={styles.newAddress}>
+              <a href="#map">
+                <Button
+                  onClick={() => {
+                    setShowMap(true);
+                  }}
+                >
+                  Set New Address
+                </Button>
+              </a>
+            </div>
+          </div>
+          {showMap && (
+            <div className={styles.map} id="map">
+              <div className={styles.mapHolder}>
+                <Map setAddress={setNewAddress} />
+              </div>
+              <div className={styles.addressInput}>
+                <IntputField label="Delivery Address" value={newAddress} />
+                <IntputField label="Nearest Landmark" />
+              </div>
+              <div className={styles.actBtn}>
+                <Button>Save</Button>
+                <Button
+                  color="error"
+                  onClick={() => {
+                    setShowMap(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </Layout>
